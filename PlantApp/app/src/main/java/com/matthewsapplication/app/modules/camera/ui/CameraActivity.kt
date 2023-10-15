76d25b7fcc6eb046c9fc8deb55a9d94734832cb8
FloorMatt89 <86789069+FloorMatt89@users.noreply.github.com>
2023-10-15 06:26:16 -0400

    Update CameraActivity.kt

--- PlantApp/app/src/main/java/com/matthewsapplication/app/modules/camera/ui/CameraActivity.kt
+++ PlantApp/app/src/main/java/com/matthewsapplication/app/modules/camera/ui/CameraActivity.kt
@@ -4,22 +4,37 @@ import android.content.Context
 import android.content.Intent
 import android.graphics.Bitmap // Import the Bitmap class
 import android.os.Bundle
+import android.os.Environment
 import android.provider.MediaStore
+import android.util.Log
 import android.widget.ImageView
 import android.widget.Toast
 import androidx.activity.viewModels
 import android.widget.Button
+import java.io.FileOutputStream
+import java.io.IOException
 import com.matthewsapplication.app.R
 import com.matthewsapplication.app.appcomponents.base.BaseActivity
 import com.matthewsapplication.app.databinding.ActivityCameraBinding
 import com.matthewsapplication.app.modules.camera.data.viewmodel.CameraVM
 import com.matthewsapplication.app.modules.mainmenu.ui.MainmenuActivity
+import java.io.File
 
 class CameraActivity : BaseActivity<ActivityCameraBinding>(R.layout.activity_camera) {
   private val viewModel: CameraVM by this.viewModels<CameraVM>()
 
   private val REQUEST_CODE = 22 // Replaced the static constant with a property
-
+  fun saveBitmapAsJPEG(bitmap: Bitmap, filePath: String){
+    val file = File(filePath)
+    try{
+      val stream = FileOutputStream(file)
+      bitmap.compress(Bitmap.CompressFormat.JPEG,100,stream)
+      stream.flush()
+      stream.close()
+    }catch(e:IOException){
+      e.printStackTrace()
+    }
+  }
   override fun onInitialized() {
     viewModel.navArguments = intent.extras?.getBundle("bundle")
     val cameraVM: CameraVM = viewModel
@@ -38,18 +53,63 @@ class CameraActivity : BaseActivity<ActivityCameraBinding>(R.layout.activity_cam
     binding.btnScanPlant.setOnClickListener {
       val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
       startActivityForResult(cameraIntent, REQUEST_CODE)
+
+
     }
   }
 
+
+
+
+// ...
+
   override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
     if (requestCode == REQUEST_CODE && resultCode == android.app.Activity.RESULT_OK) {
-      val photo: Bitmap? = data?.extras?.get("data") as? Bitmap // Added a safe cast
+      val photo = data?.extras?.get("data") as? Bitmap
       binding.imageview1.setImageBitmap(photo)
+
+      // Get the external storage directory
+      val externalStorageDirectory = Environment.getExternalStorageDirectory()
+
+      // Create a relative path within the external storage directory
+      val relativePath = "/Users/matthewsegura/Downloads/output" // Replace with your desired directory name
+      val myDir = File(externalStorageDirectory, relativePath)
+      myDir.mkdirs()
+
+      // Create a file for the image
+      val file = File(myDir, "photo.jpg")
+
+      try {
+        val stream = FileOutputStream(file)
+        photo?.compress(Bitmap.CompressFormat.JPEG, 100, stream)
+        stream.flush()
+        stream.close()
+
+        val successMessage = "Image saved to ${file.absolutePath}"
+        Toast.makeText(this, successMessage, Toast.LENGTH_SHORT).show()
+
+        // Log the success message
+        Log.d("CameraActivity", successMessage)
+      } catch (e: IOException) {
+        e.printStackTrace()
+        val errorMessage = "Failed to save image"
+        Toast.makeText(this, errorMessage, Toast.LENGTH_SHORT).show()
+
+        // Log the error message
+        Log.e("CameraActivity", errorMessage)
+      }
     } else {
-      Toast.makeText(this, "Cancelled", Toast.LENGTH_SHORT).show()
+      val cancelledMessage = "Cancelled"
+      Toast.makeText(this, cancelledMessage, Toast.LENGTH_SHORT).show()
+
+      // Log the cancellation message
+      Log.d("CameraActivity", cancelledMessage)
+
       super.onActivityResult(requestCode, resultCode, data)
     }
   }
+
+
 
   companion object {
     const val TAG: String = "CAMERA_ACTIVITY"
